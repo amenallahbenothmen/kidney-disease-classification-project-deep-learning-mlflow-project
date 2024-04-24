b8a2515d258b9e860bd8f2ddf35aa89040d026b3
amenallahbenothmen <amenallah.benothmen@supcom.tn>
2024-04-24 21:50:40 +0100

    model preparation done

--- README.md
+++ README.md
@@ -12,11 +12,3 @@ Update the main.py
 Update the dvc.yaml
 app.py
 
-#how to run 
-
-###STEPS:
-
-clone the repositry 
-
-### step_1 create conda env 
-conda create -n gitenv python=3.8 -y
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from CNN_Classifier_Project import logger
 from CNN_Classifier_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from CNN_Classifier_Project.pipeline.satge_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 STAGE_NAME = "DATA Ingestion stage"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -11,3 +11,12 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Prepare base model stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
